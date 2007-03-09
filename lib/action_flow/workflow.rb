c8311c71a65b706911bd313a3c127513dc2c7f31
pat <pat@82013aef-fb27-0410-99a1-c1893322ecab>
2007-03-09 20:39:08 +0000

    Allow add_step to take a block again execute checks to see if the controller responds to max_step_reached= before just calling it
    git-svn-id: svn://evang.eli.st/public/public/plugins/action_flow@21 82013aef-fb27-0410-99a1-c1893322ecab

--- lib/action_flow/workflow.rb
+++ lib/action_flow/workflow.rb
@@ -11,6 +11,7 @@ module ActionFlow
         @steps << new_step
       else
         step = "#{new_step}_step".classify.constantize
+        step.module_eval { define_method(:execute, &action) } if block_given?
         @steps << step
       end
     end
@@ -18,7 +19,7 @@ module ActionFlow
     def execute(controller, max_step = nil)
       @steps.each_with_index do |s, i|
         controller.extend s
-        controller.max_step_reached = i + 1
+        controller.max_step_reached = i + 1 if controller.respond_to?(:max_step_reached=)
         if !controller.passes? || (!max_step.nil? && max_step.to_i == (i + 1))
           controller.execute
           return true
